7320dd8e07ccce5f932e4f79552abc074e8acbab
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-03-09 16:32:40 -0500

    players and bets

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
+import TwoClubs from '../assets/cards/2C.jpg';
+
 class Board extends Component {
 
   render(){
     return (
-      <h1 style={styles.header}>Poker.Tech</h1>
+      <React.Fragment>
+        <h1 style={styles.header}>Poker.Tech</h1>
+        <div style={styles.board}>
+          <img src={TwoClubs} draggable="false" style={styles.card}/>
+          <img src={TwoClubs} draggable="false" style={styles.card}/>
+          <img src={TwoClubs} draggable="false" style={styles.card}/>
+
+          <img src={TwoClubs} draggable="false" style={styles.card}/>
+          <img src={TwoClubs} draggable="false" style={styles.card}/>
+        </div>
+
+        <div style={styles.players}>
+          <div>
+            <h1 style={styles.bet}>Bet: 150</h1>
+            <h1 style={styles.player}>P1 - 1500</h1>
+          </div>
+          <div>
+            <h1 style={styles.bet}>Bet: 150</h1>
+            <h1 style={styles.player}>P2 - 1500</h1>
+          </div><div>
+            <h1 style={styles.bet}>Bet: 150</h1>
+            <h1 style={styles.player}>P3 - 1500</h1>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
-
 }
 
 const styles = {
@@ -17,6 +42,31 @@ const styles = {
     marginTop: '5vh',
     color: '#FFAAEA',
     textShadow: '1px 1px #001514',
+  },
+  board: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    width: '50vw',
+    margin: '0 auto'
+  },
+  card: {
+    height: '10em',
+    width: '7.5em',
+    borderRadius: '10px',
+    border: '2px solid #001514'
+  },
+  players: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    bottom: 0,
+    position: 'absolute',
+    width: '100vw'
+  },
+  player: {
+    textAlign: 'center'
+  },
+  bet: {
+    textAlign: 'center'
   }
 }
 
