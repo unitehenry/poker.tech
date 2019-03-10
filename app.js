a32453efb485f6c950b16ea9ee9d5039ee324a01
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-03-10 10:37:33 -0400

    port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'app/build')));
 
-app.get('/:id', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + 'app/build/index.html'))
 })
 
