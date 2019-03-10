cab732309bdb724add7a6417eef5382c27386d02
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-03-10 10:35:31 -0400

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
 const path = require('path');
 
 const cors = require('cors');
